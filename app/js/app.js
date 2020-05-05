e7052c626641fa85428e238fcbfee07936404971
xselsior <m.tolstov2013@yandex.ru>
2020-05-05 20:36:40 +0300

    После типографики и меню

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 	// Custom JS
+ /*
+ Smooth Scrolling thanks to:
+ https://css-tricks.com/snippets/jquery/smooth-scrolling/
+ */ 
 
+$(function() {
+	$('a[href*="#"]:not([href="#"])').click(function() {
+	  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		if (target.length) {
+		  $('html, body').animate({
+			scrollTop: target.offset().top
+		  }, 1000);
+		  return false;
+		  }
+		}
+	  });
+	});
 });
